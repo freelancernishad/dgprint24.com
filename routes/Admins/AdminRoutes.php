9d95480ffdc4925a6a55eb543b1c4ceecce85206
freelancernishad <freelancernishad123@gmail.com>
2025-10-29 20:19:54 +0600

    update

--- routes/Admins/AdminRoutes.php
+++ routes/Admins/AdminRoutes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 // Load ProductsRoutes
-if (file_exists($ProductsRoutes = __DIR__.'/Products/ProductsRoutes.php')) {
+if (file_exists($ProductsRoutes = __DIR__.'/products/ProductsRoutes.php')) {
     require $ProductsRoutes;
 }
 
